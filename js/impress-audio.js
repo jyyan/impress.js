febaee5453ebdd9e64a7a2ed622673f0fdd2627a
jyyan <jyyan.tw@gmail.com>
2015-05-28 13:52:18 +0800

    Add keyboard event : can using <esc> / <enter> key to pause & resume auto playing.

--- js/impress-audio.js
+++ js/impress-audio.js
@@ -2,14 +2,14 @@
 https://github.com/danielsimons1/impress-audio
 Author: Daniel Simons
 Author Email: daniel.simons1@gmail.com
-Version: 1.0.0
+Version: 1.0.1
 License: Free General Public License (GPL)
 */
 (function(document, $, undefined) {
   'use strict';
 
   var isPlaying, waitTimeout ,
-    audio,video,media,currPlaying,
+    $media,currPlaying,
     impressObj = impress(),
     impressGoto = impressObj.goto;
   var TimerID;
@@ -21,13 +21,13 @@ License: Free General Public License (GPL)
       currPlaying.pause();
     }
 
-    media = $currSlide.find('audio,video');
+    $media = $currSlide.find('audio,video');
 
     // make it play audio first then try to play the video if exist
-    if(media[0]) {
-      currPlaying = media.first()[0];
+    if($media[0]) {
+      currPlaying = $media.first()[0];
       currPlaying.play();
-      media.each(function(){
+      $media.each(function(){
         $(this)[0].removeEventListener('ended'); // unbind event if its exist
         $(this)[0].addEventListener('ended',function() {
           var nextPlay = $(this).next()[0]
@@ -49,7 +49,7 @@ License: Free General Public License (GPL)
     }
 
     clearTimeout(TimerID);
-    if(!media[0] && waitTimeout > 0 ){
+    if(!$media[0] && waitTimeout > 0 && isPlaying){
       TimerID = setTimeout(function(){impressObj.goto($currSlide.next())}, waitTimeout );
     }
   });
@@ -58,6 +58,14 @@ License: Free General Public License (GPL)
     isPlaying = true;
     waitTimeout = parseInt(SecTimeout)*1000 ;
     this.goto(0);
+  };
+  impressObj.resume = function() {
+    isPlaying = true;
+    currPlaying.play();
+  };
+  impressObj.pause= function() {
+    isPlaying = false;
+    currPlaying.pause();
   };
 
   impressObj.goto = function($el) {
@@ -68,9 +76,22 @@ License: Free General Public License (GPL)
     }
   }
 
-// TODO:
-// 1. add keydown Listener for <enter> / <esc> for play(resume) / pause event
-//
+  document.addEventListener("keyup", function ( event ) {
+    if ( event.keyCode === 27 ||  event.keyCode === 13 ) {
+      switch( event.keyCode ) {
+        case 27: // esc
+        case 13:  // enter
+          if(isPlaying){
+            impressObj.pause();
+          }else{
+            impressObj.resume();
+          }
+        break;
+      }
+
+      event.preventDefault();
+    }
+  }, false);
 
 })(document, jQuery);
 
